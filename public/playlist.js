e2aebfe4756cb5aea903342f5c59870be45ce959
Aziza <ooozizo@gmail.com>
2021-02-28 19:58:10 +0300

    add page playlist

---
+++ public/playlist.js
@@ -0,0 +1,79 @@
+//Halsey - Still Learning
+let mySongs1 = document.getElementById("mySongs1");
+let icon1 = document.getElementById("icon1");
+
+icon1.onclick = function() {
+    if (mySongs1.paused) {
+        mySongs1.play();
+    }else 
+    {mySongs1.pause();
+   
+    }
+}
+
+//STARSET - Monster
+let mySongs2 = document.getElementById("mySongs2");
+let icon2 = document.getElementById("icon2");
+
+icon1.onclick = function() {
+    if (mySongs2.paused) {
+        mySongs2.play();
+    }else 
+    {mySongs2.pause();
+   
+    }
+}
+
+//Christina Perri - A Thousand Years
+let mySongs3 = document.getElementById("mySongs3");
+let icon3 = document.getElementById("icon3");
+
+icon3.onclick = function() {
+    if (mySongs3.paused) {
+        mySongs3.play();
+    }else 
+    {mySongs3.pause();
+   
+    }
+}
+
+//Astro Bot Rescue Mission-Soundtrack-I am ASTRO BOT-By Kenneth C M Young
+let mySongs4 = document.getElementById("mySongs4");
+let icon4 = document.getElementById("icon4");
+
+icon4.onclick = function() {
+    if (mySongs4.paused) {
+        mySongs4.play();
+    }else 
+    {mySongs4.pause();
+   
+    }
+}
+
+//Culture Code - Make Me Move (feat. Karra)
+let mySongs5 = document.getElementById("mySongs5");
+let icon5 = document.getElementById("icon5");
+
+icon1.onclick = function() {
+    if (mySongs5.paused) {
+        mySongs5.play();
+    }else 
+    {mySongs5.pause();
+   
+    }
+}
+
+//Rimshox Ft. Veela - Another Dare
+let mySongs6 = document.getElementById("mySongs6");
+let icon6 = document.getElementById("icon6");
+
+icon6.onclick = function() {
+    if (mySongs6.paused) {
+        mySongs6.play();
+    }else 
+    {mySongs6.pause();
+   
+    }
+}
+   
+
